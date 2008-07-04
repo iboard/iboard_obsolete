2caac408fb0434fc9c723d6d45786fb70a537c1f
Andi Altendorfer <andi@iboard.cc>
2008-07-05 00:11:48 +0200

    textile_editor plugin added to script/install_plugins

--- script/install_plugins.sh
+++ script/install_plugins.sh
@@ -12,3 +12,6 @@ sudo gem install will_paginate
 sudo gem install activemerchant
 sudo gem install money
 
+# textile_editor_helper
+script/plugin install http://svn.webtest.wvu.edu/repos/rails/plugins/textile_editor_helper/
+rake textile_editor_helper:install
